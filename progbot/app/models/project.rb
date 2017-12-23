a3731bccc70118fbb3a0c2334fa6aae2d02c3fc8
Robert Diamond <rmd6502@gmail.com>
2017-12-23 10:23:16 -0800

    add models

--- progbot/app/models/project.rb
+++ progbot/app/models/project.rb
@@ -1,3 +1,5 @@
 class Project < ApplicationRecord
-  belongs_to :lead
+  belongs_to :user, foreign_key: "lead"
+  has_and_belongs_to_many :user, through: "volunteers"
+  has_and_belongs_to_many :skills, through: "required_skills"
 end
--- progbot/app/models/user.rb
+++ progbot/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :referer
+  belongs_to :user, foreign_key: "referer"
+  has_and_belongs_to_many :skills, through: "tech_skills"
+  has_and_belongs_to_many :skills, through: "other_skills"
 end
