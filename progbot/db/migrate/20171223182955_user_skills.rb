b07a9577b4374c8125d2eca062ad410d5e79087f
Robert Diamond <rmd6502@gmail.com>
2017-12-27 13:53:23 -0800

    start adding test

--- progbot/db/migrate/20171223182955_user_skills.rb
+++ progbot/db/migrate/20171223182955_user_skills.rb
@@ -1,6 +1,6 @@
 class UserSkills < ActiveRecord::Migration[5.1]
   def change
-    create_table :users_skills, id: false do |t|
+    create_table :skills_users, id: false do |t|
         t.belongs_to :user, index: true
         t.belongs_to :skill, index: true
     end
