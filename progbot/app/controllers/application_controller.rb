c601e7db88913a042c75a7980d32da1d350ea5b9
Robert Diamond <rmd6502@gmail.com>
2017-12-28 16:08:35 -0800

    update app controller

--- progbot/app/controllers/application_controller.rb
+++ progbot/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  include Secrets
   protect_from_forgery with: :exception
+
+  def verify_token() {
+    assert params[:token] == @VERIFICATION_TOKEN
+  }
+
 end
