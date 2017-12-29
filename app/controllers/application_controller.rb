0aa81dbc4cc08095874073247a654980145fac59
Robert Diamond <rmd6502@gmail.com>
2017-12-28 21:58:30 -0800

    add skills

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  include Secrets
   protect_from_forgery with: :exception
-
-  def verify_token()
-    if params[:token] != @VERIFICATION_TOKEN
-      raise "verification token mismatch"
-    end
-  end
-
 end
