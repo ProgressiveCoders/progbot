9e86fa64ab9dd6d00a7bca686519425d628a6411
Robert Diamond <rmd6502@gmail.com>
2017-12-30 12:09:26 -0800

    s

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -44,7 +44,8 @@ class SlackController < APIController
         result_str += skill_list.join(", ") + "\n"
       end
       logger.debug "results #{result_str}"
-      result_body["attachments"] = [{"title": "#{results.length} Users Found", "text": result_str}]
+      s = results.count != 1 ? "s" : ""
+      result_body["attachments"] = [{"title": "#{results.count} User#{s} Found", "text": result_str}]
     else
       logger.debug "count 0"
       result_body["attachments"] = [{"title": "No users found", "text": "No results"}]
