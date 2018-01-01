a2724b5ed14c1741a8b2ebffe8af065dd582747d
Ryan T. Hosford <tad.hosford@gmail.com>
2018-01-01 12:21:13 -0600

    Relocate ApplicationHelper code to POROs and test
      - Typically helper methods are used in views
      - Reorganizing the code a little makes it more testable
      - Finish implementing project search
        - Search for users with skills needed by a given project
        - Refactors and tests with same approach as taken for users search

--- test/controllers/slack_controller_test.rb
+++ test/controllers/slack_controller_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class SlackControllerTest < ActionDispatch::IntegrationTest
   test "should get search" do
-    post slack_search_url, params: { text: "c++, ruby" }
+    post slack_search_url, params: { text: "c++, ruby", response_url: "/" }
     assert_response :success
   end
 
   test "should get project_search" do
-    post slack_project_search_url
+    post slack_project_search_url, params: { text: "Project1", response_url: "/" }
     assert_response :success
   end
-
 end
