54dee20a07e8f55819dd9b7d3cb5084ab74826e5
Robert Diamond <rmd6502@fb.com>
2018-01-03 12:57:39 -0800

    use helper methods

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -3,26 +3,28 @@ require 'slack_results'
 class SlackController < APIController
 
   def search
-    if params[:text].length > 0
+    if params[:text].empty?
+      render json: { response_type: "ephemeral", text: "Please specify at least one skill" }
+    else
       # tell the user to cool their heels
       render json: { response_type: "ephemeral", text: "performing search for: #{params[:text]}" }
       SlackResults.new(UserSearch, params).call
-    else
-      render json: { response_type: "ephemeral", text: "Please specify at least one skill" }
     end
   end
 
   def project_skills_search
-    if params[:text].length > 0
+    if params[:text].empty?
+      render json: { response_type: "ephemeral", text: "Please specify at least one skill" }
+    else
       render json: { response_type: "ephemeral", text: "performing search for: #{params[:text]}" }
       res = SlackResults.new(ProjectSearch, params).call
-    else
-      render json: { response_type: "ephemeral", text: "Please specify at least one skill" }
     end
   end
 
   def project_search
-    if params[:text].length > 0
+    if params[:text].empty?
+      render json: { response_type: "ephemeral", text: "Please specify a project" }
+    else
       project = Project.where(name: params[:text]).first
       if project == nil
         render json: { response_type: "ephemeral", text: "project not found!" }
@@ -30,8 +32,6 @@ class SlackController < APIController
         render json: { response_type: "ephemeral", text: "performing search: #{params[:text]}" }
         SlackResults.new(ProjectUsersSearch, params.merge({ project: project })).call
       end
-    else
-      render json: { response_type: "ephemeral", text: "Please specify a project" }
     end
   end
 end
