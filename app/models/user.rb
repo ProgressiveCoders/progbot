3b3900157ba31e3eea0dfb4d9967509e4051ec40
Jake Varghese <jake@compose.io>
2018-05-05 17:14:05 -0500

    adding leftover columns and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tech_skills, -> { where tech: true }, class_name: "Skill"
   has_and_belongs_to_many :non_tech_skills, -> { where tech: false }, class_name: "Skill"
   
-  validates_presence_of :name, :email
+  validates_presence_of :name, :email, :slack_username, :location, :hear_about_us, :join_reason
+  validates_acceptance_of :read_code_of_conduct
+  
+  after_create :send_slack_notification
   
   
   def tech_skill_names=(skill_names)
@@ -13,5 +16,10 @@ class User < ApplicationRecord
   
   def non_tech_skill_names=(skill_names)
     self.non_tech_skill_ids = Skill.where(name: skill_names.split(", ")).pluck(:id)
+  end
+  
+  
+  def send_slack_notification
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230202533) do
+ActiveRecord::Schema.define(version: 20180505210538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,8 @@ ActiveRecord::Schema.define(version: 20171230202533) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "optin"
+    t.text "hear_about_us"
+    t.text "verification_urls"
     t.index ["referer_id"], name: "index_users_on_referer_id"
   end
 
