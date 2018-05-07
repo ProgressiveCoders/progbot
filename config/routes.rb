5282d499cd48745345658687531fe061860573ff
sukey <sdklos@gmail.com>
2018-05-06 23:10:44 -0400

    added home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'users#welcome'
   post 'slack/search'
 
   post 'slack/project_search'
@@ -10,6 +11,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'users#new'
-  resource :users, except: [:index] 
+  resource :users, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
