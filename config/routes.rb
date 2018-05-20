5079c3b3749d264e6d6cabd37c209ecc8321caed
sukey <sdklos@gmail.com>
2018-05-20 18:03:22 -0400

    add approved user form and is_approved workflow

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'users#new'
+
   resource :users, except: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
