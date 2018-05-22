f29629731c82642e5277002b735f7b0134663631
Susanna Klosko <sdklos@gmail.com>
2018-05-22 19:00:02 -0400

    Update api_controller.rb

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,7 +5,6 @@ class APIController < ActionController::API
 
   before_action :verify_token
   def verify_token
-    binding.pry
     if params[:token] != @@SLACK_VERIFICATION_TOKEN
       raise "verification token mismatch"
     end
