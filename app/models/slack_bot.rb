8bc570dab1853cfea6d2df68421aed8f591e3e9b
sukey <sdklos@gmail.com>
2018-06-07 06:57:32 -0400

    small syntax errors

--- app/models/slack_bot.rb
+++ app/models/slack_bot.rb
@@ -1,12 +1,12 @@
 class SlackBot
   class << self
-    def post_to_recruitment(user, testing = false)
+    def post_to_recruitment(user, testing = true)
       client.chat_postMessage(recruitment_params(user, testing))
     end
-    
+
     def recruitment_params(user, testing = false)
-      { 
-        channel: testing ? "#progbot" : '#recruitment', 
+      {
+        channel: testing ? "#progbot" : '#recruitment',
         attachments: [
           {
             pretext: 'New User Signup',
@@ -18,13 +18,13 @@ class SlackBot
         ]
       }.merge(default_params)
     end
-    
+
     def client
       @client = Slack::Web::Client.new
       @client.auth_test
       @client
     end
-    
+
     def default_params
       { username: "Botty The ProgCode Bot"}
     end
