03f40ff315cce524cd2163961b993bc553cfeb33
sukey <sdklos@gmail.com>
2018-09-06 23:06:58 -0400

    fix association between user and array of projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   include ProjectConstants
-  # belongs_to :lead, class_name: "User"  # We are going to use arrays.
+
   has_and_belongs_to_many :volunteers, class_name: "User", join_table: "projects_volunteers"
   has_and_belongs_to_many :skills
 
