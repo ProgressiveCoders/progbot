364c75aef0a0ca37b0c4c5d67d5377d372f0f52d
sukey <sdklos@gmail.com>
2018-12-20 21:55:52 -0500

    continue to make progress on airtable project import

--- app/models/airtable_project.rb
+++ app/models/airtable_project.rb
@@ -15,7 +15,7 @@ end
 class AirtableProject < Airrecord::Table
   self.base_key = "appUZignaw9kIHqTR"
   self.table_name = "Projects"
-  # has_many :slack_channels, class: 'AirtableChannelList', column: "Slack Channel"
+  has_many :slack_channels, class: 'AirtableChannelList', column: "Slack Channel"
   # has_many :progcode_coordinators, class: 'AirtableBaseManager', column: "ProgCode Coordinator(s)"
   # has_many :project_lead_slack_ids, column: "Project Lead Slack ID"
 end
