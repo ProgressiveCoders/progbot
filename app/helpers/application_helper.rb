a06ec9747a9c7255a15ca89003f57475ad676f15
sukey <sdklos@gmail.com>
2018-12-31 14:00:27 -0500

    add errors to import and change status to array

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,5 +26,9 @@ module ApplicationHelper
     klass_name.compact.join(" ")
   end
 
+  def format_skills(skills = @tech_skills)
+    skills.map { |skill| {"id": skill.id, "value": skill.name } }
+  end
+
 
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,3 @@
 module ProjectsHelper
-    def format_skills(skills = @tech_skills)
-        skills.map { |skill| {"id": skill.id, "value": skill.name } }
-    end
+
 end
