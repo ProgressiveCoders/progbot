1984c095f12ffddcb41de2ba292a465ae1ab9e97
sukey <sdklos@gmail.com>
2019-01-05 13:23:25 -0500

    slight change to the way skills are treated

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -7,7 +7,7 @@ class Skill < ApplicationRecord
   has_and_belongs_to_many :projects, join_table: "projects_skills"
 
   scope :tech_skills, -> () { where(tech: true).order("name ASC") }
-  scope :non_tech_skills, -> () { where(tech: false).order("name ASC") }
+  scope :non_tech_skills, -> () { where(tech: !true).order("name ASC") }
   scope :match_skill, -> (str) { where("lower(name) = lower(?)", str_or_arr) }
   scope :match_skills, -> (arr) { where("lower(name) IN (?)", arr.map(&:downcase)) }
 
