16e781cb098c28fc158799e02b000a357cb6d4e6
sukey <sdklos@gmail.com>
2019-05-16 22:37:15 -0400

    remove pry

--- app/admin/volunteerings.rb
+++ app/admin/volunteerings.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 ActiveAdmin.register Volunteering do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
