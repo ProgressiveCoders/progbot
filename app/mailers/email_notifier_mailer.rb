1bbf719a849f99e3e6bbcf4322970e2ef88aaddb
sukey <sdklos@gmail.com>
2019-07-18 19:55:22 -0400

    refactor email notifications and remove slack username from validation

--- app/mailers/email_notifier_mailer.rb
+++ app/mailers/email_notifier_mailer.rb
@@ -1,7 +1,7 @@
 class EmailNotifierMailer < ApplicationMailer
     default from: 'admin@progcode.org'
 
-    layout "mailer"
+    layout 'email_notifier'
 
     def new_volunteer_email
         mail(to:'sdklos@gmail.com', subject: 'We Received Your Application')
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tech_skills, -> { where tech: true }, class_name: "Skill"
   has_and_belongs_to_many :non_tech_skills, -> { where tech: false }, class_name: "Skill"
 
-  validates_presence_of :name, :email, :slack_username, :location, :hear_about_us, :join_reason
+  validates_presence_of :name, :email, :location, :hear_about_us, :join_reason
   validates_acceptance_of :read_code_of_conduct
 
   has_many :volunteerings
