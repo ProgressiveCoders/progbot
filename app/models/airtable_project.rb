e50337dc458859406fa4ddc4b9dbda8fe8e74052
sukey <sdklos@gmail.com>
2020-07-04 23:11:37 -0400

    put in environmental variables for airtable table and column names

--- app/models/airtable_project.rb
+++ app/models/airtable_project.rb
@@ -3,15 +3,15 @@ Airrecord.api_key = ENV['AIRTABLE_API_KEY']
 
 class AirtableChannelList < Airrecord::Table
   self.base_key = ENV['AIRTABLE_BASE_KEY']
-  self.table_name = "Channel List"
+  self.table_name = ENV['AIRTABLE_CHANNEL_LIST_TABLE_NAME']
 end
 
 class AirtableProject < Airrecord::Table
   self.base_key = ENV['AIRTABLE_BASE_KEY']
-  self.table_name = "Projects"
-  belongs_to :slack_channel, class: 'AirtableChannelList', column: "Slack Channel"
-  has_many :master_channel_lists, class: 'AirtableChannelList', column: "Master Channel List"
-  has_many :progcode_coordinators, class: 'AirtableBaseManager', column: "ProgCode Coordinator(s)"
-  has_many :project_leads, class: 'AirtableUser', column: "Project Lead"
-  has_many :members, class: 'AirtableUser', column: "Team Member"
+  self.table_name = ENV['AIRTABLE_PROJECT_TABLE_NAME']
+  belongs_to :slack_channel, class: 'AirtableChannelList', column: ENV['AIRTABLE_PROJECT_SLACK_CHANNEL_COLUMN']
+  has_many :master_channel_lists, class: 'AirtableChannelList', column: ENV['AIRTABLE_PROJECT_MASTER_CHANNEL_COLUMN']
+  has_many :progcode_coordinators, class: 'AirtableBaseManager', column: ENV['AIRTABLE_PROJECT_COORDINATORS_COLUMN']
+  has_many :project_leads, class: 'AirtableUser', column: ENV['AIRTABLE_PROJECT_LEADS_COLUMN']
+  has_many :members, class: 'AirtableUser', column: ENV['AIRTABLE_PROJECT_MEMBERS_COLUMN']
 end
