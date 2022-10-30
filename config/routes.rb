88c5401ff5dcbb089d475a06e0a08367a7ad3868
Robert Diamond <rmd6502@gmail.com>
2022-10-30 12:51:46 -0700

    hack for rake db:create

--- config/routes.rb
+++ config/routes.rb
@@ -62,9 +62,10 @@ Rails.application.routes.draw do
 
   post 'admin/users/bulk_post', to: 'admin/users#bulk_post', as: :admin_users_bulk_post
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
+  # uncomment these lines after rake db:create is finished
+  #devise_for :admin_users, ActiveAdmin::Devise.config
 
-  ActiveAdmin.routes(self)
+  #ActiveAdmin.routes(self)
 
 
 
